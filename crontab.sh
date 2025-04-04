069574344facabf6dce6b4139e39f79de2f1bcdc
Ubuntu <ubuntu@ip-172-31-40-178.eu-north-1.compute.internal>
2025-04-04 16:31:35 +0000

    Bon type de fichiers

--- crontab.sh
+++ crontab.sh
@@ -1,2 +1,2 @@
-*/5 * * * * /usr/bin/python3 /home/ubuntu/Projetc_PGL/scrapped_data.py >> /home/ubuntu/Projetc_PGL/log.txt 2>&1
+*/5 * * * * /usr/bin/python3 /home/ubuntu/Projetc_PGL/scrapped_data.sh >> /home/ubuntu/Projetc_PGL/log.txt 2>&1
 
