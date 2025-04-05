36cf1df730eef25e0562091f258e064a9283ff8e
OliviaAvard <oavard@gmail.com>
2025-04-05 23:40:04 +0200

    changement de chemin pour stocker les données

--- scrapped_data.sh
+++ scrapped_data.sh
@@ -7,4 +7,4 @@ html=$(curl -s "$API")
 price=$(echo "$html" | grep -oP '"pi-network":\{"usd":\K[0-9.]+')
 time=$(date "+%Y-%m-%d %H:%M:%S")
 date_only=$(date "+%Y-%m-%d")
-echo "$time, $price, $date_only" >> pi_network_prices.csv
+echo "$time, $price, $date_only" >> /home/ubuntu/Projetc_PGL/pi_network_prices.csv
